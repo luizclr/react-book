53cc63f731658ab10ecdb280070069ca7f82e421
Luiz Cláudio <luiz.lclr@gmail.com>
2019-08-23 15:46:33 -0300

    Add scss loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import './index.scss';
+
 class App extends Component {
   render() {
     return <h1>React book</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+      },
     ],
   },
 };
