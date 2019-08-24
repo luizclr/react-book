d223a98c58f08ba7f3aaa381a049294cd650c9c5
Luiz Cláudio <luiz.lclr@gmail.com>
2019-08-24 07:56:51 -0300

    Create container component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import Header from './components/Header';
+import Container from './components/Conteiner';
 
 import './style/index.scss';
 
@@ -10,6 +11,9 @@ class App extends Component {
     return (
       <div>
         <Header title="React book" />
+        <Container>
+          <p>hello</p>
+        </Container>
       </div>
     );
   }
