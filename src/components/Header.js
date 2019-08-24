701fe39d6a029bbf4212242485df919e08403984
Luiz Cláudio <luiz.lclr@gmail.com>
2019-08-24 08:31:40 -0300

    Improve project architecture - style and templates folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../style/Header.scss';
+import '../../style/components/Header.scss';
 
 const Header = ({ title }) => (
   <div className="header">
