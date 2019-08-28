bab832993fd6ed71ffd2bcc196b057de856f46b3
Luiz Cláudio <luiz.lclr@gmail.com>
2019-08-27 22:22:56 -0300

    Update webpack config files

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = {
+  ...common,
+  mode: 'production',
+  output: {
+    ...common.output,
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'templates', 'index.ejs'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeRedundantAttributes: true,
+      },
+    }),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: false,
+      minimize: true,
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
