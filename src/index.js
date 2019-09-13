4254f3bf07223ada8d9881c7ae4739cf0a0ee61b
Luiz Cláudio <luiz.lclr@gmail.com>
2019-09-12 21:37:17 -0300

    Add Post component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import Header from './components/header/Header';
+import Post from './components/post/Post';
 import Container from './components/base/Conteiner';
 
 import './style/index.scss';
@@ -12,7 +13,7 @@ class App extends Component {
       <div>
         <Header title="React book" />
         <Container>
-          <p>hello</p>
+          <Post />
         </Container>
       </div>
     );
