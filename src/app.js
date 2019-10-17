700bb244a7d2ec479111e09cbe81fa90f7285954
Luiz Claudio <luiz.lclr@gmail.com>
2019-10-17 00:56:01 -0300

    Add redux to header component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import store from './store';
 
 import Header from './components/header/Header';
 import PostList from './components/postList/PostList';
+import Sidebar from './components/sidebar/Sidebar';
 import './style/index.scss';
 
 export default class App extends Component {
@@ -13,9 +14,9 @@ export default class App extends Component {
       <Provider store={store}>
         <Header title="Reactbook" />
         <div className="page-wrapper">
-          <div className="navbar">
+          <Sidebar>
             <p>nav</p>
-          </div>
+          </Sidebar>
           <div className="container">
             <PostList />
           </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Creators as NavigationActions } from '../../store/ducks/navigation';
+
+import {
+  addEventListener, removeEventListener, debounce, isMobile,
+} from '../../utils/index';
 
 import '../../style/components/Header.scss';
 
 class Header extends Component {
-  state = {
-    showToggleButton: false,
-  };
-
   componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
+    this.updateDimensions();
+    addEventListener('resize', debounce(this.updateDimensions));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
+    removeEventListener('resize', debounce(this.updateDimensions));
   }
 
   updateDimensions = () => {
-    if (window.innerWidth >= 993) {
-      this.setState({ showToggleButton: false });
-    } else {
-      this.setState({ showToggleButton: true });
-    }
+    const { toggleNav } = this.props;
+    toggleNav(isMobile());
   };
 
-  toggleMenu = () => {};
-
   render() {
-    const { showToggleButton } = this.state;
-    const { title } = this.props;
+    const { title, isOn, toggleNav } = this.props;
     return (
       <div className="header">
-        {showToggleButton && (
-          <button type="button" onClick={this.toggleMenu}>
-            {JSON.stringify(showToggleButton)}
+        {isOn && (
+          <button type="button" className="header__button" onClick={() => toggleNav(false)}>
+            MENU
           </button>
         )}
+
         <h1>{title}</h1>
       </div>
     );
@@ -44,6 +44,17 @@ class Header extends Component {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  isOn: PropTypes.bool.isRequired,
+  toggleNav: PropTypes.func.isRequired,
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  isOn: state.navigation.isOn,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(NavigationActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Header);
